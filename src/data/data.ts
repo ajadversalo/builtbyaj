9e90cb3be416fea54003917a393dcc402bf77625
ajadversalo <ajadvers@gmail.com>
2025-09-10 22:19:19 -0700

    comment out

--- src/data/data.ts
+++ src/data/data.ts
@@ -89,16 +89,16 @@ export const productListCentra = [
     },
 ];
 
-const skillsByCategory = {
-  Frontend: [
-    { name: "React", image: "/images/react.png" },
-    { name: "Next.js", image: "/images/nextjs.png" },
-  ],
-  Backend: [
-    { name: "C#", image: "/images/csharp.png" },
-    { name: ".NET", image: "/images/dotnet.png" },
-  ],
-};
+//const skillsByCategory = {
+//  Frontend: [
+//    { name: "React", image: "/images/react.png" },
+//    { name: "Next.js", image: "/images/nextjs.png" },
+//  ],
+//  Backend: [
+//    { name: "C#", image: "/images/csharp.png" },
+//    { name: ".NET", image: "/images/dotnet.png" },
+//  ],
+//};
 
 export const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // eslint-disable-line
 
